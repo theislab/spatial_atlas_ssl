e80170cce8154ed0e65b76b600f64943e037d803
lendres-ai <leopoldendres@msn.com>
2023-07-28 22:10:45 +0200

    changes batch size to use main laoder batch size

--- spatialSSL/Utils.py
+++ spatialSSL/Utils.py
@@ -6,8 +6,8 @@ def split_dataloader(loader, split_percent=(0.8, 0.1, 0.1)):
     train_data, val_data, test_data = random_split(loader.dataset, split_percent)
 
     # Create data loaders for each set
-    train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
-    val_loader = DataLoader(val_data, batch_size=64, shuffle=False)
-    test_loader = DataLoader(test_data, batch_size=64, shuffle=False)
+    train_loader = DataLoader(train_data, batch_size=loader.batch_size, shuffle=True)
+    val_loader = DataLoader(val_data, batch_size=loader.batch_size, shuffle=False)
+    test_loader = DataLoader(test_data, batch_size=loader.batch_size, shuffle=False)
 
     return train_loader, val_loader, test_loader
