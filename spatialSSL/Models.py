8c8e63766373fd63a2887de739a800420c4fdb44
lendres-ai <leopoldendres@msn.com>
2023-08-01 21:20:29 +0200

    add more complex model

--- spatialSSL/Models.py
+++ spatialSSL/Models.py
@@ -12,3 +12,17 @@ class GCN(nn.Module):
         x = self.conv1(x, edge_index).relu()
         x = self.lin1(x)
         return x
+
+
+class GCN3layer(nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super().__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.lin1 = nn.Linear(hidden_channels, out_channels)
+
+    def forward(self, x: Tensor, edge_index: Tensor) -> Tensor:
+        x = self.conv1(x, edge_index).relu()
+        x = self.conv2(x, edge_index).relu()
+        x = self.lin1(x)
+        return x
